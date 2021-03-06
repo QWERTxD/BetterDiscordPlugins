b3c0ee903c7706d196adcfc92ce2dc1747078aa6
QWERTxD <77858402+QWERTxD@users.noreply.github.com>
2021-03-06 17:29:03 +0200

    Update QuickMessages.plugin.js

--- QuickMessages/QuickMessages.plugin.js
+++ QuickMessages/QuickMessages.plugin.js
@@ -5,8 +5,6 @@
  * @website https://github.com/QWERTxD/BetterDiscordPlugins/tree/main/QuickMessages
  */
 
-/// <reference types="bandagedbd/bdapi" />
-
 const request = require("request");
 const fs = require("fs");
 const path = require("path");
@@ -20,49 +18,60 @@ const config = {
                 discord_id: "678556376640913408",
             }
         ],
-        version: "1.0.31",
+        version: "1.1.0",
         description: "Save messages to quickly send them later, when you need.",
         github: "https://github.com/QWERTxD/BetterDiscordPlugins/tree/main/QuickMessages",
         github_raw: "https://raw.githubusercontent.com/QWERTxD/BetterDiscordPlugins/main/QuickMessages/QuickMessages.plugin.js",
-        changelog: [
+    },
+            changelog: [
             {
-                type: 'added',
-                title: 'Added',
-                items: ['Changelog']
+                title: "Fixed",
+                type: "fixed",
+                items: [
+                    "Emojis will now be saved. (Thanks to dav1312 for the report https://github.com/QWERTxD/BetterDiscordPlugins/issues/4)"
+                ],
+            },
+            {
+                title: "Improvements",
+                type: "improved",
+                items: [
+                    "Rewritten the Patch of the context menu."
+                ],
             }
         ]
-    }
 };
 
 module.exports = !global.ZeresPluginLibrary ? class {
+    constructor() {
+        this._config = config;
+    }
+
     getName() {
         return config.info.name;
     }
 
     getAuthor() {
-        return config.info.authors[0].name;
-    }
-
-    getVersion() {
-        return config.info.version;
+        return config.info.authors.map(author => author.name).join(", ");
     }
 
     getDescription() {
         return config.info.description;
     }
 
-
+    getVersion() {
+        return config.info.version;
+    }
 
     load() {
-        
         BdApi.showConfirmationModal("Library plugin is needed",
             `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
                 confirmText: "Download",
                 cancelText: "Cancel",
                 onConfirm: () => {
                     request.get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", (error, response, body) => {
-                        if (error)
+                        if (error) {
                             return electron.shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        }
 
                         fs.writeFileSync(path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body);
                     });
@@ -70,12 +79,12 @@ module.exports = !global.ZeresPluginLibrary ? class {
             });
     }
 
-    start() {  }
+    start() { }
 
     stop() { }
 } : (([Plugin, Library]) => {
     const { DiscordModules, WebpackModules, Patcher, DiscordContextMenu, Settings } = Library;
-    const { SettingPanel, SettingGroup, Dropdown } = Settings;
+    const { SettingPanel, SettingGroup } = Settings;
 
     const { React } = DiscordModules;
     function configArrayPush(name, key, data) {
@@ -140,6 +149,8 @@ module.exports = !global.ZeresPluginLibrary ? class {
             Patcher.unpatchAll();
         }
 
+
+
         forceUpdate() {
             updateMessages();
             Patcher.unpatchAll();
@@ -147,59 +158,55 @@ module.exports = !global.ZeresPluginLibrary ? class {
         }
 
         patchTextAreaContextMenus() {
-            var en = true;
-            const TextAreaContextMenus = WebpackModules.findAll(m => m.default && m.default.displayName.includes("SlateTextAreaContextMenu"))[0];
+            var shouldPaste = true;
+            const SlateTextAreaContextMenu = WebpackModules.find(m => m.default?.displayName === "SlateTextAreaContextMenu");
+            const CloseCircle = BdApi.findModuleByDisplayName('CloseCircle');
+            const ComponentDispatch = BdApi.findModuleByProps("ComponentDispatch").ComponentDispatch;
+            const ChannelTextAreaContainer = WebpackModules.find(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer").type;
                 const children = [];                                 
                 messages.forEach(message => {
                     children.push(DiscordContextMenu.buildMenuItem({
                         label: message,
                         action: _ => {
-                            en === true ? BdApi.findModuleByProps("ComponentDispatch").ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {content: message}) : ''
+                           if(!shouldPaste) return;
+                           ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", {content: message})
                         },
-                        hint: React.createElement('button', {
-                            class: 'clearStatusButton-1Mxs1q da-clearStatusButton button-38aScr da-button lookBlank-3eh9lL colorBrand-3pXr91 grow-q77ONN da-grow',
+                        hint: React.createElement(CloseCircle, {
                             onClick: _ => {
-                                en = false;
+                                shouldPaste = false;
                                 configArrayRemove("QuickMessages", 'messages', message);
-                                updateMessages();
-                                Patcher.unpatchAll();
-                                this.patchTextAreaContextMenus();
-                                BdApi.showToast(`Successfully removed Quick Message!`, {type: 'success'})
+                                this.forceUpdate();
+                                BdApi.showToast(`Successfully removed Quick Message!`, {type: 'success'});
                             },
-                            },
-                            React.createElement('svg', {
-                            class: 'clearStatusIcon-3-MDNF da-clearStatusIcon',
-                        }, React.createElement('path', {
-                            fill: 'currentColor',
-                            d: 'M7.02799 0.333252C3.346 0.333252 0.361328 3.31792 0.361328 6.99992C0.361328 10.6819 3.346 13.6666 7.02799 13.6666C10.71 13.6666 13.6947 10.6819 13.6947 6.99992C13.6947 3.31792 10.7093 0.333252 7.02799 0.333252ZM10.166 9.19525L9.22333 10.1379L7.02799 7.94325L4.83266 10.1379L3.89 9.19525L6.08466 6.99992L3.88933 4.80459L4.832 3.86259L7.02733 6.05792L9.22266 3.86259L10.1653 4.80459L7.97066 6.99992L10.166 9.19525Z',
-                        })))
+                            width: '15px',
+                            height: '15px'
+                        })
                     }));
                 })
                 
-                const patch = (thisObject, [props], returnValue) => {
-                returnValue.props.children.push(
+                const patch = (_, [props], ret) => {
+                    ret.props.children.push(
                     DiscordContextMenu.buildMenuItem({
                         type: "separator"
                     }),
                     DiscordContextMenu.buildMenuItem({
                         label: "Save as Quick Message",
                         disabled: props.editor.containerRef.current.textContent.slice(0, -1) == props.editor.props.placeholder,
-                        action:_ => {
-                            const save = props.editor.containerRef.current.innerText.replace(/\n\n/g, '\n').replace(/^\s+|\s+$/g, '');
-                            configArrayPush("QuickMessages", "messages", save);
-                            this.forceUpdate()
-                            BdApi.showToast(`Successfully created new Quick Message!`, {type: 'success'})
-
+                        action:_ => {       
+                            Patcher.after(ChannelTextAreaContainer, "render", (_, [{ textValue }], ret) => {
+                            configArrayPush("QuickMessages", "messages", textValue);
+                            this.forceUpdate();
+                            BdApi.showToast(`Successfully created new Quick Message!`, {type: 'success'});
+                        });
                         }
                     }),
                     DiscordContextMenu.buildMenuItem({
                         label: "Quick Messages",
                         children: children.length > 0 ? children : DiscordContextMenu.buildMenuItem({label: 'None'}),
-                    })
-                );
-            };
- 
-                    Patcher.after(TextAreaContextMenus, "default", patch);
+                      }));
+                    };
+                    Patcher.after(SlateTextAreaContextMenu, "default", patch);
+
             }
             
             
