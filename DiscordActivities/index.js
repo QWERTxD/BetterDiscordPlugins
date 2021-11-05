4a076d3d545ddbc9e284e5bfc904693fb530f75c
jlortiz0 <jlortiz0@users.noreply.github.com>
2021-11-05 16:53:10 -0700

    Add new applications
    The game server added more apps, so I added them to the list. I also added a thing to automatically update the list from the game server if possible.
    
    Since the endpoint 401s if you aren't in the server, a user must be in the game server to update the list, so I added a backup list. But if that list gets out of date, there could be problems, so I added a toast to notify any interested maintainers that the backup list needs to be updated.

--- DiscordActivities/index.js
+++ DiscordActivities/index.js
@@ -1,4 +1,4 @@
-import { Patcher, WebpackModules } from '@zlibrary';
+import { Patcher, WebpackModules, DiscordAPI, Toasts } from '@zlibrary';
 import { GuildStore } from '@zlibrary/discord';
 import BasePlugin from '@zlibrary/plugin';
 
@@ -24,17 +24,39 @@ export default class DiscordActivities extends BasePlugin {
 
     enableExperiment() {
         const applicationIds = [
-                activities.POKER_NIGHT_APPLICATION_ID,
-                activities.END_GAME_APPLICATION_ID,
-                activities.FISHINGTON_APPLICATION_ID,
-                activities.WATCH_YOUTUBE_PROD_APP_ID,
-                activities.WATCH_YOUTUBE_DEV_APP_ID
+            "755827207812677713",
+            "832012774040141894",
+            "832013003968348200",
+            "878067389634314250",
+            "879863976006127627",
+            "879863686565621790",
+            "852509694341283871",
+            "880218394199220334",
+            activities.END_GAME_APPLICATION_ID,
+            activities.FISHINGTON_APPLICATION_ID,
+            activities.WATCH_YOUTUBE_DEV_APP_ID
         ];
-        Patcher.instead(activitiesExperiment, "getEnabledAppIds", function (e) {
-            return applicationIds;
-        });
-        Patcher.instead(activitiesExperiment, "isActivitiesEnabled", function (e) {
-            return true;
-        });
+        const gameGuildId = "831646372519346186";
+        var isInGameGuild = DiscordAPI.Guild.fromId(gameGuildId);
+        if (isInGameGuild == undefined) {
+            Patcher.instead(activitiesExperiment, "getEnabledAppIds", (function() {
+                return applicationIds;
+            }));
+        } else {
+            WebpackModules.getByProps("fetchEmbeddedActivities").fetchEmbeddedActivities("831646372519346186");
+            Patcher.before(activitiesExperiment, "getEnabledAppIds", (function(_, args) {
+                args[0] = gameGuildId;
+            }));
+            Patcher.after(activitiesExperiment, "getEnabledAppIds", (function(_, __, ret) {
+                if (ret[ret.length - 1] != activities.WATCH_YOUTUBE_DEV_APP_ID) {
+                    ret.push(activities.END_GAME_APPLICATION_ID);
+                    ret.push(activities.FISHINGTON_APPLICATION_ID);
+                    ret.push(activities.WATCH_YOUTUBE_DEV_APP_ID);
+                    if (ret.length !== applicationIds.length || !ret.every(function(value, index) { return value === applicationIds[index]})) {
+                        Toasts.show("DiscordActivities: redownloaded activities list", "warning");
+                    }
+                }
+            }));
+        }
     }
 }
