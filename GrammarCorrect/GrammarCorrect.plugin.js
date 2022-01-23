1a8db248462df87a036d3042f0de23909aca65ce
MichaelGK <39009683+MichaelDavidGK@users.noreply.github.com>
2022-01-23 03:12:48 +0000

    Fix for new Discord update
    Changed textArea in line 172

--- GrammarCorrect/GrammarCorrect.plugin.js
+++ GrammarCorrect/GrammarCorrect.plugin.js
@@ -169,7 +169,7 @@ function buildPlugin([BasePlugin, PluginApi]) {
 								duration: 200,
 								intensity: 3
 							});
-							const textArea = document.querySelector(".textArea-12jD-V");
+							const textArea = document.querySelector(".textArea-2CLwUE");
 							const editor = getInternalInstance(textArea).return.stateNode.editorRef;
 							editor.moveToRangeOfDocument();
 							editor.delete();
