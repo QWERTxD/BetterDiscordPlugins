5440ae595fae4d631889f82383695417193901db
HypedDomi <domi@bambus.me>
2022-05-24 17:13:48 +0200

    Cleaned up Dev Branch

--- AssignBadges/index.js
+++ AssignBadges/index.js
@@ -1,4 +1,4 @@
-import { Patcher, PluginUtilities, Utilities, WebpackModules, Toasts } from "@zlibrary";
+import { Patcher, PluginUtilities, Utilities, WebpackModules, Toasts, DCM} from "@zlibrary";
 import BasePlugin from "@zlibrary/plugin";
 import { MenuGroup, MenuItem } from "@discord/contextmenu";
 import { UserStore } from "@zlibrary/discord";
@@ -24,27 +24,31 @@ const User = WebpackModules.getByPrototypes("getAvatarURL");
 const BadgeList = WebpackModules.getByProps("BadgeSizes").default;
 
 const boosts = [
-	{ id: "boost1", value: 0 << 0, name: "Booster - 1 Month", time: 1 },
-	{ id: "boost2", value: 0 << 0, name: "Booster - 2 Months", time: 2 },
-	{ id: "boost3", value: 0 << 0, name: "Booster - 3 Months", time: 3 },
-	{ id: "boost4", value: 0 << 0, name: "Booster - 6 Months", time: 6 },
-	{ id: "boost5", value: 0 << 0, name: "Booster - 9 Months", time: 9 },
-	{ id: "boost6", value: 0 << 0, name: "Booster - 1 Year", time: 12 },
-	{ id: "boost7", value: 0 << 0, name: "Booster - 1 Year and 3 Months", time: 15 },
-	{ id: "boost8", value: 0 << 0, name: "Booster - 1 Year and 6 Months", time: 18 },
-	{ id: "boost9", value: 0 << 0, name: "Booster - 2 Years", time: 24 }
+	{ id: "boost1", value: 0 << 0, name: "Booster - 1 Month", time: 1.04 },
+	{ id: "boost2", value: 0 << 0, name: "Booster - 2 Months", time: 2.04 },
+	{ id: "boost3", value: 0 << 0, name: "Booster - 3 Months", time: 3.04 },
+	{ id: "boost4", value: 0 << 0, name: "Booster - 6 Months", time: 6.04 },
+	{ id: "boost5", value: 0 << 0, name: "Booster - 9 Months", time: 9.04 },
+	{ id: "boost6", value: 0 << 0, name: "Booster - 1 Year", time: 12.04 },
+	{ id: "boost7", value: 0 << 0, name: "Booster - 1 Year and 3 Months", time: 15.04 },
+	{ id: "boost8", value: 0 << 0, name: "Booster - 1 Year and 6 Months", time: 18.04 },
+	{ id: "boost9", value: 0 << 0, name: "Booster - 2 Years", time: 24.04 }
 ]
 
-const UserContextMenus = WebpackModules.findAll(m => m.default?.displayName.endsWith("UserContextMenu"));
-const UserGenericContextMenu = WebpackModules.find(m => m.default?.displayName === "UserGenericContextMenu")
-const BotTag = WebpackModules.getByDisplayName("BotTag");
+const BotTag = WebpackModules.getByProps("BotTagTypes").default;
 const MessageAuthor = WebpackModules.find(m => m.default.toString().indexOf("userOverride") > -1)
 const NameTag = WebpackModules.find(m => m.default.displayName === "DiscordTag");
 const MemberListItem = WebpackModules.find(m => m.default.displayName === "MemberListItem");
 
-UserContextMenus.push(UserGenericContextMenu);
 
 export default class AssignBadges extends BasePlugin {
+	promises = {
+		cancelled: false,
+		cancel() {
+			this.cancelled = true;
+		},
+	};
+	patches = [];
 	onStart() {
 		this.patchUserContextMenus();
 		this.patchUserFlagGetter();
@@ -136,105 +140,169 @@ export default class AssignBadges extends BasePlugin {
 		})
 	}
 
-	patchUserContextMenus() {
-		for(const UserContextMenu of UserContextMenus) {
-			Patcher.after(UserContextMenu, "default", (_this, [props], ret) => {
-				const settings = this.getSettings();
-				const tree = Utilities.getNestedProp(ret, "props.children.props.children");
-				const userBadges = getFlags.default({user: UserStore.getUser(props.user.id)}).map(badge => badge?.key);
-				tree.splice(7, 0, 
-				<MenuGroup>
-					<MenuItem
-					id="assign-badge"
-					label="Manage Badges"
-					children={
-						[UserFlags.map(flag => {
-							const [state, setState] = React.useState((settings?.[props.user.id]?.hasOwnProperty(flag.id) ? settings?.[props.user.id]?.[flag.id] : ~userBadges.indexOf(flag.key)));
-							return <MenuCheckboxItem
-							id={flag.id}
+	async patchUserContextMenus() {
+		const getMenu = (props) => {
+			const settings = this.getSettings();
+			const propsUser = UserStore.getUser(props);
+			props = {user: propsUser};
+			const userBadges = getFlags
+				.default({ user: propsUser })
+				.map((badge) => badge?.key);
+			
+			const [selectedBoost, setSelectedBoost] = React.useState(settings[props.user.id]?.boost);
+
+			return (<MenuItem
+			id="assign-badge"
+			key="assign-badge"
+			label="Manage Badges"
+			children={
+				[UserFlags.map(flag => {
+					const [state, setState] = React.useState((settings?.[props.user.id]?.hasOwnProperty(flag.id) ? settings?.[props.user.id]?.[flag.id] : ~userBadges.indexOf(flag.key)));
+					return <MenuCheckboxItem
+					id={flag.id}
+					label={
+						flag.id !== "EARLY_VERIFIED_BOT" ? 
+						<div className={classes?.container}>
+						<BadgeList user={this.fakeUser(flag.value)} premiumSince={flag.id === "PREMIUM" ? new Date(0) : null} size={2}/>
+							{flag.name}
+						</div>
+						:
+						<div className={classes?.container}>
+							{<BotTag verified={true}/>}
+						</div> 
+					}
+					checked={state}
+					action={() => {
+						const user = settings[props.user.id] || {};
+						user[flag.id] = !state;
+						settings[props.user.id] = user;
+						this.saveSettings(settings);
+						setState(!state);
+					}}
+				/>
+				}),
+				<MenuItem
+				id="boosts"
+				label={
+					<div className={classes?.container}>
+					<BadgeList user={this.fakeUser(0)} premiumGuildSince={new Date(Date.now() - months(3) - day)} size={2}/>
+						Boosts
+					</div>
+				}>
+				{
+					[
+						boosts.map(boost => {
+							return <MenuRadioItem
+							id={boost.id}
+							checked={selectedBoost === boost.id}
 							label={
-								flag.id !== "EARLY_VERIFIED_BOT" ? 
-								<div className={classes?.container}>
-								<BadgeList user={this.fakeUser(flag.value)} premiumSince={flag.id === "PREMIUM" ? new Date(0) : null} size={2}/>
-									{flag.name}
-								</div>
-								:
-								<div className={classes?.container}>
-									{<BotTag verified={true}/>, flag.name}
-								</div> 
+							<div className={classes?.container}>
+							<BadgeList user={this.fakeUser(0)} premiumGuildSince={new Date(Date.now() - months(boost.time) - day)} size={2}/>
+								{boost.name}
+							</div>
 							}
-							checked={state}
 							action={() => {
 								const user = settings[props.user.id] || {};
-								user[flag.id] = !state;
+								user.boost = boost.id;
+								setSelectedBoost(boost.id);
 								settings[props.user.id] = user;
 								this.saveSettings(settings);
-								setState(!state);
-							}}
-						/>
-						}),
-						<MenuItem
-						id="boosts"
-						label={
-							<div className={classes?.container}>
-							<BadgeList user={this.fakeUser(0)} premiumGuildSince={new Date(Date.now() - months(3) - day)} size={2}/>
-								Boosts
-							</div>
-						}>
-						{
-							[
-								boosts.map(boost => {
-									return <MenuRadioItem
-									id={boost.id}
-									checked={settings[props.user.id]?.boost === boost.id}
-									label={
-									<div className={classes?.container}>
-									<BadgeList user={this.fakeUser(0)} premiumGuildSince={new Date(Date.now() - months(boost.time) - day)} size={2}/>
-										{boost.name}
-									</div>
-									}
-									action={() => {
-										const user = settings[props.user.id] || {};
-										user.boost = boost.id;
-										settings[props.user.id] = user;
-										this.saveSettings(settings);
-								}}
-							/>
-							}),
-							<MenuGroup>
-								<MenuItem
-									label="Reset Boost Preferences"
-									id="reset-boosts"
-									color="colorDanger"
-									action={() => {
-										delete settings[props.user.id]?.boost;
-										this.saveSettings(settings);
-										Toasts.success(`Successfully cleared boost preferences for <strong>${props.user}</strong>!`)
-									}}
-								/>					
-							</MenuGroup>
-						]
-						}
-						</MenuItem>,
-						<MenuGroup>
-						<MenuItem
-							color="colorDanger"
-							label="Reset Preferences"
-							id="reset"
-							action={() => {
-								delete settings[props.user.id];
-								this.saveSettings(settings);
-								settings[props.user.id] = {};
-								Toasts.success(`Successfully cleared preferences for <strong>${props.user}</strong>!`)
-							}}
-						/>
-						</MenuGroup>
-						]}
+						}}
 					/>
+					}),
+					<MenuGroup>
+						<MenuItem
+							label="Reset Boost Preferences"
+							id="reset-boosts"
+							color="colorDanger"
+							action={() => {
+								delete settings[props.user.id]?.boost;
+								this.saveSettings(settings);
+								Toasts.success(`Successfully cleared boost preferences for <strong>${props.user}</strong>!`)
+							}}
+						/>					
+					</MenuGroup>
+				]
+				}
+				</MenuItem>,
+				<MenuGroup>
+				<MenuItem
+					color="colorDanger"
+					label="Reset Preferences"
+					id="reset"
+					action={() => {
+						delete settings[props.user.id];
+						this.saveSettings(settings);
+						settings[props.user.id] = {};
+						Toasts.success(`Successfully cleared preferences for <strong>${props.user}</strong>!`)
+					}}
+				/>
 				</MenuGroup>
-				)
-			})
+				]}
+			/>)
 		}
+
+		//Credits to Strencher for this (https://github.com/Strencher/BetterDiscordStuff/blob/ea3fc6ff0cc9d6a4fed511f103d400d5618188ec/Copier/Copier.plugin.js#L1133-L1192)
+		function filterContext(name) {
+            const shouldInclude = ["page", "section", "objectType"];
+            const notInclude = ["use", "root"];
+            const isRegex = name instanceof RegExp;
+
+            return (module) => {
+                const string = module.toString({});
+                const getDisplayName = () => Utilities.getNestedProp(module({}), "props.children.type.displayName");
+
+                return !~string.indexOf("return function")
+                    && shouldInclude.every(s => ~string.indexOf(s))
+                    && !notInclude.every(s => ~string.indexOf(s))
+                    && (isRegex ? name.test(getDisplayName()) : name === getDisplayName())
+            }
+        }
+
+		const patched = new Set();
+		const REGEX = /user.*contextmenu|useUserRolesItems/i;
+		const filter = filterContext(REGEX);
+		const search = async () => {
+			const Menu = await DCM.getDiscordMenu(
+				(m) => {
+					if (patched.has(m)) return false;
+                    if (m.displayName != null) return REGEX.test(m.displayName);
+
+                    return filter(m);
+				}
+			);
+			if (this.promises.cancelled) return;
+
+			let original = null;
+			function wrapper(props) {
+				const rendered = original.call(this, props);
+				try {
+					const children = Utilities.findInReactTree(rendered, Array.isArray);
+					const user = props.user || UserStore.getUser(props.channel?.getRecipientId?.());
+					if (!children || !user || children.some(c => c && c.key === "assign-badge")) return rendered;
+					children.splice(7, 0, getMenu(user.id)); 
+				} catch (error) {
+					cancel();
+					console.error("Error in context menu patch:", error);
+				}
+				return rendered;
+			}
+			const cancel = Patcher.after(Menu, "default", (...args) => {
+				const [, , ret] = args;
+				const contextMenu = Utilities.getNestedProp(ret, "props.children");
+				if (!contextMenu || typeof contextMenu.type !== "function") return;
+
+				original ??= contextMenu.type;
+				wrapper.displayName ??= original.displayName;
+				contextMenu.type = wrapper;
+			});
+
+			
+			patched.add(Menu.default);
+			search();
+		};
+
+		search();
 	}
 
 	fakeUser(flags) {
