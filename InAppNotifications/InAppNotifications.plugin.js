c4fefa19840bc1d16ce75a9311734ce15db238e2
Pokefan5 <marcel.brandl@pm.me>
2022-10-13 16:51:35 +0200

    cleanup

--- InAppNotifications/InAppNotifications.plugin.js
+++ InAppNotifications/InAppNotifications.plugin.js
@@ -225,21 +225,20 @@ module.exports = !global.ZeresPluginLibrary
        * As taken from https://discord.com/developers/docs/resources/channel#channel-object-channel-types
        * since `WebpackModules.getByProps("GUILD_TEXT")` no longer works
        */
-      var tmpChannelTypes = [];
-      tmpChannelTypes["GUILD_TEXT"] = 0;
-      tmpChannelTypes["DM"] = 1;
-      tmpChannelTypes["GUILD_VOICE"] = 2;
-      tmpChannelTypes["GROUP_DM"] = 3;
-      tmpChannelTypes["GUILD_CATEGORY"] = 4;
-      tmpChannelTypes["GUILD_ANNOUNCEMENT"] = 5;
-      tmpChannelTypes["ANNOUNCEMENT_THREAD"] = 10;
-      tmpChannelTypes["PUBLIC_THREAD"] = 11;
-      tmpChannelTypes["PRIVATE_THREAD"] = 12;
-      tmpChannelTypes["GUILD_STAGE_VOICE"] = 13;
-      tmpChannelTypes["GUILD_DIRECTORY"] = 14;
-      tmpChannelTypes["GUILD_FORUM"] = 15;
-
-      const ChannelTypes = tmpChannelTypes;
+      const ChannelTypes = {
+        "GUILD_TEXT" : 0,
+        "DM" : 1 ,
+        "GUILD_VOICE" : 2 ,
+        "GROUP_DM" : 3 ,
+        "GUILD_CATEGORY" : 4 ,
+        "GUILD_ANNOUNCEMENT" : 5 ,
+        "ANNOUNCEMENT_THREAD" : 10 ,
+        "PUBLIC_THREAD" : 11 ,
+        "PRIVATE_THREAD" : 12 ,
+        "GUILD_STAGE_VOICE" : 13 ,
+        "GUILD_DIRECTORY" : 14 ,
+        "GUILD_FORUM" : 15 ,
+      };
 
       const MuteStore = WebpackModules.getByProps("isSuppressEveryoneEnabled");
       const isMentioned = { isRawMessageMentioned: getByString("suppressEveryone") };
